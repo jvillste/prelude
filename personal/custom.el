e047bc0e31898df371ee7cc0292f269dc977ff7e
Jukka Villstedt <jukka.villstedt@gmail.com>
2013-12-28 13:41:37 +0200

    x

---
+++ personal/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("f3d2144fed1adb27794a45e61166e98820ab0bbf3cc7ea708e4bf4b57447ee27" default)))
+ '(highlight-symbol-foreground-color nil)
+ '(highlight-symbol-idle-delay 0.1)
+ '(ispell-highlight-face (quote flyspell-incorrect))
+ '(menu-bar-mode nil)
+ '(prelude-whitespace nil)
+ '(volatile-highlights-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight-symbol-face ((t (:background "forest green" :foreground "black"))))
+ '(region ((t (:background "dark green")))))
